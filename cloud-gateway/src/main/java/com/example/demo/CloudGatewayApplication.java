71c297fde25eabb77ef3a6be013c784c5c7849ac
saurav yadav <sauravyadav1958@gmail.com>
2024-11-04 11:33:15 +0530

    cloud gateway changes

--- cloud-gateway/src/main/java/com/example/demo/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/example/demo/CloudGatewayApplication.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix // Allows to implement circuit breakers
+@CircuitBreaker(name = "myCircuitBreaker") // Allows to implement circuit breakers
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
